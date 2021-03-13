fc3f019e74923e5e20d9498698b969c8d557e379
Ahmed Ramadan <ahmed.r.sharaf3@gmail.com>
2021-03-13 21:37:14 +0200

    update output for instance

--- episode-2/main.tf
+++ episode-2/main.tf
@@ -24,10 +24,20 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-038f1ca1bd58a5790"
   instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleInstance"
   }
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
 }
